341075964871b6fc902ff69a657f858cf9718bf9
sandeep <sandybalu001@gmail.com>
2024-03-29 21:57:05 +0530

    first commit

---
+++ src/main/java/com/example/employeesalarydeduction/EmployeeSalaryDeductionApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeesalarydeduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSalaryDeductionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeSalaryDeductionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/employeesalarydeduction/EmployeeSalaryDeductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeesalarydeduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSalaryDeductionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
